e8af37b1d572bcddc83df52e22100523284d56d0
DouglasGavioli <douglasbgavioli@hotmail.com>
2023-10-10 16:36:34 -0300

    Alteracao nas patentes e ordenacao das mesmas conforme a patente

--- src/pages/Integrantes/index.tsx
+++ src/pages/Integrantes/index.tsx
@@ -8,100 +8,24 @@ import ProgressBar from "../../components/ProgressBar";
 
 const ArrayLevel = [
     {
-        img: "./trainee.png",
-        lvl: "CDT"
+        img: "./soldadon.png",
+        lvl: "SD"
     },
     {
-        img: "./recruit.png",
-        lvl: "RCT"
+        img: "./cabo.png",
+        lvl: "CB"
     },
     {
-        img: "./private.png",
-        lvl: "SDR"
-    },
-    {
-        img: "./private_first.png",
-        lvl: "SPC"
-    },
-    {
-        img: "./croporal.png",
-        lvl: "CAB"
-    },
-    {
-        img: "./sergeant.png",
+        img: "./sargento.png",
         lvl: "SGT"
     },
     {
-        img: "./ssg01.png",
-        lvl: "SGT¹"
+        img: "./tenente.png",
+        lvl: "TEN"
     },
     {
-        img: "./ssg02.png",
-        lvl: "SGT²"
-    },
-    {
-        img: "./ssg03.png",
-        lvl: "SGT3"
-    },
-    {
-        img: "./sfc01.png",
-        lvl: "SG"
-    },
-    {
-        img: "./msg03.png",
-        lvl: "SGM"
-    },
-    {
-        img: "./csm01.png",
-        lvl: "SMC"
-    },
-    {
-        img: "./2lt01.png",
-        lvl: "ST"
-    },
-    {
-        img: "./1lt01.png",
-        lvl: "PT"
-    },
-    {
-        img: "./cpt01.png",
+        img: "./capitao.png",
         lvl: "CAP"
-    },
-    {
-        img: "./maj01.png",
-        lvl: "MAJ"
-    },
-    {
-        img: "./ltc01.png",
-        lvl: "TNC"
-    },
-    {
-        img: "./col01.png",
-        lvl: "CEL"
-    },
-    {
-        img: "./star_lv1.png",
-        lvl: "GB"
-    },
-    {
-        img: "./star_lv2.png",
-        lvl: "GD"
-    },
-    {
-        img: "./star_lv3.png",
-        lvl: "TG"
-    },
-    {
-        img: "./star_lv4.png",
-        lvl: "GEN"
-    },
-    {
-        img: "./star_lv5.png",
-        lvl: "GE"
-    },
-    {
-        img: "./logo.png",
-        lvl: "GM"
     },
 ]
 
@@ -121,32 +45,35 @@ export default function Integrantes() {
         <div className="dua-members">
             <TitlePage title="Integrantes" subtitle="Integrantes e suas respectivas patentes(baseadas na experiência adquirida dentro da Divisão Urutu)" />
             <div className="dua-members__cards">
-                {members?.map((member, index) => (
-                    <div className="dua-members__cards__content" key={index}>
-                        <div className="dua-members__cards__content-card">
-                            <div className="dua-members__cards__content-card-image">
-                                <img src={member.file} alt="Card" />
-                            </div>
-                            <div className="dua-members__cards__content-card-descriptions">
-                                <div className="dua-members__cards__content-card-descriptions-text">
-                                    <div className="dua-members__cards__content-card-descriptions-text-content">
-                                        <h2>{member.name}</h2>
-                                        <p>{member.insta}</p>
-                                    </div>
-                                    <div className="dua-members__cards__content-card-descriptions-text-imgLvl">
-                                        <img src={ArrayLevel[member?.level]?.img} alt={ArrayLevel[member.level]?.lvl} title={ArrayLevel[member.level]?.lvl} />
-                                        <p>{ArrayLevel[member.level]?.lvl}</p>
-                                    </div>
-
+                {members
+                    ?.slice()
+                    .sort((a, b) => (a.level > b.level ? -1 : 1))
+                    ?.map((member, index) => (
+                        <div className="dua-members__cards__content" key={index}>
+                            <div className="dua-members__cards__content-card">
+                                <div className="dua-members__cards__content-card-image">
+                                    <img src={member.file} alt="Card" />
                                 </div>
-                                <div className="dua-members__cards__content-card-descriptions-xp">
-                                    <p>XP</p>
-                                    <ProgressBar percentage={member.xp} />
+                                <div className="dua-members__cards__content-card-descriptions">
+                                    <div className="dua-members__cards__content-card-descriptions-text">
+                                        <div className="dua-members__cards__content-card-descriptions-text-content">
+                                            <h2>{member.name}</h2>
+                                            <p>{member.insta}</p>
+                                        </div>
+                                        <div className="dua-members__cards__content-card-descriptions-text-imgLvl">
+                                            <img src={ArrayLevel[member?.level]?.img} alt={ArrayLevel[member.level]?.lvl} title={ArrayLevel[member.level]?.lvl} />
+                                            <p>{ArrayLevel[member.level]?.lvl}</p>
+                                        </div>
+
+                                    </div>
+                                    <div className="dua-members__cards__content-card-descriptions-xp">
+                                        <p>XP</p>
+                                        <ProgressBar percentage={member.xp} />
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
             </div>
         </div>
     )
