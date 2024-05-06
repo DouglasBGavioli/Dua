b6e4b2866587c9c4064ac48ba2eaf2efa09a04dc
DouglasGavioli <douglasbgavioli@hotmail.com>
2024-05-06 15:42:16 -0300

    env adiciondo

--- src/config/firebaseClient.ts
+++ src/config/firebaseClient.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyCWTcvfDfv6T_UE3NF-5pEm5KhvEfDzVII",
-    authDomain: "divisaourutu.firebaseapp.com",
-    projectId: "divisaourutu",
-    storageBucket: "divisaourutu.appspot.com",
-    messagingSenderId: "562786537514",
-    appId: "1:562786537514:web:034c95d9438618f6737353"
-}
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
